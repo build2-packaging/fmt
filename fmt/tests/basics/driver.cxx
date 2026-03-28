90e8f0a63f393f3ce51c5b20a6f9bc6d009898c3
lyrahgames <lyrahgames@mailbox.org>
2026-03-28 14:21:19 +0100

    Remove version header generation
    Upstream does not provide a version header for its consumers.
    It would be sub-optimal to rely on it when not officially supported.
    Also, upstream offers the `FMT_VERSION` macro.

--- fmt/tests/basics/driver.cxx
+++ fmt/tests/basics/driver.cxx
@@ -1,15 +1,13 @@
 #include <cassert>
-#include <string>
 #include <chrono>
+#include <string>
 #include <vector>
 
-#include <fmt/version.h>
-#include <fmt/printf.h>
+#include <fmt/chrono.h>
+#include <fmt/color.h>
 #include <fmt/core.h>
 #include <fmt/format.h>
-#include <fmt/color.h>
-#include <fmt/chrono.h>
+#include <fmt/printf.h>
 #include <fmt/ranges.h>
 
 #include "tests.inl"
-
