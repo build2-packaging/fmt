298c7d503827096ef4a8b41a29c04360efbf2463
lyrahgames <lyrahgames@mailbox.org>
2026-04-28 14:49:58 +0200

    Remove disabled accessibility test
    Issue: #13

--- fmt/tests/basics/driver-modules.cc
+++ fmt/tests/basics/driver-modules.cc
@@ -1,16 +1,3 @@
-// Verify that fmt headers are not available if config.fmt.modules_only is true
-// @NOTE: Disabled since this is rather hard to make robust.
-// We need to install headers even in modules only mode in order to allow for
-// BMI compilation. Even though we suppress exporting of the -I paths, in the
-// case of installation to a common root prefix it will likely be the case that
-// the headers will be accessible via the general include paths anyway. Only way
-// we could reasonably prevent this would be to install to a different
-// subfolder; avoiding adding such complexity for now since modules installation
-// is far from finalized anyway.
-// #if __has_include(<fmt/version.h>) != FMT_BUILD2_HAS_HEADER
-// #error fmt headers should be available for include iff
-// config.fmt.modules_only == false #endif
-
 #if __has_include(<version>)
 #include <version>
 #endif
